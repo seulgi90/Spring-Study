7b4392a277f06055345c69868bb6bf89d1a90c7b
seulgi90 <seulgi6380@gmail.com>
2024-02-22 22:55:37 +0900

    authentication flow

--- basicsecurity/src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ basicsecurity/src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -27,7 +27,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     // ThreadLocal(Thread들끼리 공유 되는 공간이 아님)에 저장되어 참조가 가능하도록 설계
     // -> 인증 완료 시 httpSession에 저장되어 전역적인 참조 가능
     
-    // SecurityContextHolder
+    // c
     // securityContext 객체 저장 방식
     // MODE_THREADLOCAL : 스레드 당 securityContext 객체를 할당, 기본값
     // MODE_INHERITABLETHREADLOCAL :  메인 스레드와 자식 스레드에 관하여 동일한 SecurityContext 를 유지
@@ -47,5 +47,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
        // SecurityContextHolder 객체 저장 방식 설정
        SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
    }
+    // SecurityContextPersistenceFilter - HttpSecurityContextRepository : securityContext 객체의 생성, 저장, 조회
+    // 사용자의 요청을 SecurityContextPersistenceFilter 이 받아
+    // -> HttpSecurityContextRepository 에서 인증 전/후 판단하여
+    // -> 인증 전 : 새로운 컨텍스트 생성 SecurityContextHolder > securityContext = null
+    //             -> authFilter -> 인증 후 SecurityContextHolder > securityContext > Authentication 인증 객체 저장
+    //             -> SecurityContextPersistenceFilter가 Session에 SecurityContext를 저장 -> 응답
+    // -> 인증 완료 : session에 저장되어있는 SecurityContext를 찾는다 -> SecurityContextHolder > securityContext > Authentication 객체에 저장
+    
+    // Authentication Flow
+    // 로그인 요청 ->  usernamePasswordAuthenticationFilter
+    // -> Authentication id + pass 담은 인증 전 토큰 객체 생성 -> authenticate(Authentication)
+    // -> AuthenticationManager : 인증의 전반전인 관리, 실제 인증 역할 하지는 않고 AuthenticationProvider에 위임
+    // -> AuthenticationProvider : loadUserByUsername(username) 실제 인증 처리 역할, 유저 유효성 검증(패스워드 체크 등)
+    // -> UserDetailsService : findById() 유저 객체 조회, userDetails 타입으로 변환
+    // -> Repository -> UserDetailsService : User 반환
+    // -> AuthenticationProvider : UserDetails 반환
+    // -> AuthenticationManager : Authentication(UserDtails + authorities) 담은 인증 후 토큰 객체 생성
+    // -> usernamePasswordAuthenticationFilter : Authentication 반환
+    // -> SecurityContext에 인증 객체 저장
 
 }
